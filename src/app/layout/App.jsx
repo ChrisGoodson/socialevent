66835fdcf712ec70ed9ffe66d05cf522219b41e5
Chris Goodson <f.chrisgoodson@gmail.com>
2021-06-21 05:42:01 -0500

    modified app folder structure, deleted or modified unneeded code

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SocialEvent</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
 const rootEl = document.getElementById('root');
@@ -11,7 +11,7 @@ function render() {
 }
 
 if (module.hot) {
-  module.hot.accept('./App', function() {
+  module.hot.accept('./app/layout/App', function() {
     setTimeout(render);
   })
 }
