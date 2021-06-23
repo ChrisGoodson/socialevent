0de0b3af0d0aa962c32a074c3ca524e1bf9ddab4
Chris Goodson <f.chrisgoodson@gmail.com>
2021-06-23 03:54:27 -0500

    added a delete button with onclick delete event to delete event

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -18,10 +18,17 @@ export default function EventDashboard({formOpen, setFormOpen, selectEvent, sele
         selectEvent(null);
     
     }
+
+    function handleDeleteEvent(eventID){
+        setEvents(events.filter(e => e.id !== eventID));
+    }
     return(
         <Grid>
             <Grid.Column width={10}>
-                <EventList events={events} selectEvent={selectEvent} />
+                <EventList 
+                events={events} 
+                selectEvent={selectEvent}
+                deleteEvent={handleDeleteEvent} />
             </Grid.Column>
             <Grid.Column width={6}>
                 {formOpen && (
--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -3,7 +3,7 @@ import { Segment, Item, Icon, List, Button } from 'semantic-ui-react';
 import EventListAttendee from './EventListAttendee';
 
 
-export default function EventListItem({event, selectEvent}) {
+export default function EventListItem({event, selectEvent, deleteEvent}) {
     return (
         <Segment.Group>
             <Segment>
@@ -34,6 +34,11 @@ export default function EventListItem({event, selectEvent}) {
             </Segment>
             <Segment clearing> 
                 <div>{event.description}</div>
+                <Button 
+                onClick={() => deleteEvent(event.id)} 
+                color='red' 
+                floated='right' 
+                content='Delete' />
                 <Button 
                 onClick={() => selectEvent(event)} 
                 color='teal' 
