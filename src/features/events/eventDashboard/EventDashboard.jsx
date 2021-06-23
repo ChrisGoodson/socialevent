4fec2a3463b9495d4d9c34a6d4a99d8d2edfcd2f
Chris Goodson <f.chrisgoodson@gmail.com>
2021-06-23 00:30:44 -0500

    added hooks for dashboard

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Grid } from 'semantic-ui-react';
 import EventForm from '../eventForm/EventForm';
 import EventList from './EventList';
 import { sampleData } from '../../../app/api/sampleData';
 
 export default function EventDashboard(){
+    const [events, setEvents] = useState(sampleData);
     return(
         <Grid>
             <Grid.Column width={10}>
-                <EventList events={sampleData} />
+                <EventList events={events} />
             </Grid.Column>
             <Grid.Column width={6}>
             <EventForm />
--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -3,17 +3,17 @@ import { Segment, Item, Icon, List, Button } from 'semantic-ui-react';
 import EventListAttendee from './EventListAttendee';
 
 
-export default function EventListItem() {
+export default function EventListItem({event}) {
     return (
         <Segment.Group>
             <Segment>
                 <Item.Group>
                     <Item>
-                        <Item.Image size='tiny' circular src='/assets/user.png' />
+                        <Item.Image size='tiny' circular src={event.hostPhotoURL} />
                         <Item.Content>
-                            <Item.Header content='Event Title' />
+                            <Item.Header content={event.title} />
                             <Item.Description>
-                                Hosted by Linh
+                                {event.hostedby}
                             </Item.Description>
                         </Item.Content>
                     </Item>
@@ -21,19 +21,19 @@ export default function EventListItem() {
             </Segment>
             <Segment>
                 <span>
-                    <Icon name='clock' /> Date
-                    <Icon name='marker' /> Location
+                    <Icon name='clock' /> {event.date}
+                    <Icon name='marker' /> {event.venue}
                 </span>
             </Segment>
             <Segment secondary>
                 <List horizontal>
-                    <EventListAttendee />
-                    <EventListAttendee />
-                    <EventListAttendee />
+                    {event.attendees.map(attendee => (
+                        <EventListAttendee key={attendee.id} attendee={attendee} />
+                    ))}
                 </List>
             </Segment>
             <Segment clearing> 
-                <div>Description of event</div>
+                <div>{event.description}</div>
                 <Button color='teal' floated='right' content='View' />
             </Segment>
         </Segment.Group>
