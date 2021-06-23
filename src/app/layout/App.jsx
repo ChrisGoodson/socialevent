4b3db20c9c8e20348676bd38797740bcb9a1e700
Chris Goodson <f.chrisgoodson@gmail.com>
2021-06-22 19:03:56 -0500

    added a test button to test semantic

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
       <h1>SocialEvent</h1>
+
+      <button className='ui icon red button'>
+      <i className='user icon' /> Test Button
+      </button>
     </div>
   );
 }
