d55b92ec68aceb7cdff755232bdd619177208d3c
Chris Goodson <f.chrisgoodson@gmail.com>
2021-06-22 20:32:17 -0500

    created main container

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar';
+import { Container } from 'semantic-ui-react';
 
 export default function App() {
   return (
    <div> 
       <NavBar />
-      <EventDashboard />
+      <Container className='main'>
+        <EventDashboard /> 
+      </Container>
+      
 
     </div>
   );
