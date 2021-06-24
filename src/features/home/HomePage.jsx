79c3f704a826f860c9a8cab476087457d1c229de
Chris Goodson <f.chrisgoodson@gmail.com>
2021-06-23 21:35:11 -0500

    rebuilt the landing page, gave some styling and a real homepage

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Segment, Container, Header, Image, Button, Icon } from 'semantic-ui-react';
 
 export default function HomePage() {
     return (
-        <h1>homepage</h1>
+        <Segment inverted textAlign='center' vertical className='masthead'>
+            <Container>
+                <Header as='h1' inverted>
+                    <Image size='massive' src='assets/logo.png' style={{maginBottom: 12}}/>
+                    SocialEvents
+                </Header>
+                <Button size='huge' inverted>
+                    Let's go!
+                    <Icon name='right arrow' inverted/>
+                </Button>
+            </Container>
+        </Segment>
     )
 }
