53a4dc7a5f8b3ada08b375c7b40e2d20e77d2571
Chris Goodson <f.chrisgoodson@gmail.com>
2021-06-23 21:19:52 -0500

    changed static buttons and onclick functions to navlinks to match routes

--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 export default function NavBar({setFormOpen}) {
     return (
         <Menu inverted fixed='top'>
             <Container>
-                <Menu.Item header>
+                <Menu.Item as={NavLink} to= '/' header>
                     <img src='/assets/logo.png' alt='logo' style={{marginRight: 21}}/> 
                         SocialEvents
                 </Menu.Item>
-                <Menu.Item name='Events' />
-                <Menu.Item>
-                    <Button onClick={()=>setFormOpen(true)} positive inverted content=' Create Event'/>
+                <Menu.Item  as={NavLink} to='events' name='Events' />
+                <Menu.Item  as={NavLink} to= '/createEvent' >
+                    <Button positive inverted content='Create Event'/>
                 </Menu.Item>
                 <Menu.Item position='right'>
                     <Button basic inverted content='Login' />
